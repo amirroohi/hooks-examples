4347726e767172739167d19794f4f5d9746c611b
amirroohi <amirroohi@yahoo.com>
2024-01-27 17:23:36 +0330

    created app by using useContext

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import "./App.css";
+import Form from "./components/Form";
+import { ThemeProvider, useTheme } from "./context/ThemeContext";
+
+export default function MyApp() {
+  return (
+    <ThemeProvider>
+      <div>
+        <Form />
+        <ToggleTheme />
+      </div>
+    </ThemeProvider>
+  );
+}
+
+function ToggleTheme() {
+  const { theme, setTheme } = useTheme();
+
+  return (
+    <label>
+      <input
+        type="checkbox"
+        checked={theme === "dark"}
+        onChange={(e) => {
+          setTheme(e.target.checked ? "dark" : "light");
+        }}
+      />
+      Use dark mode
+    </label>
+  );
+}
